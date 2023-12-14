e29e57bea24936f87499023acfd8b9395a0b0469
Lyron Co Ting Keh <lyronctk@stanford.edu>
2023-12-14 11:30:49 -0500

    cleaning up CardGame.sol midway

--- contracts/scripts/forge_create_local.sh
+++ contracts/scripts/forge_create_local.sh
@@ -2,6 +2,6 @@
 source ../../.env
 
 # Deploy contract to local chain
-forge create src/Draw.sol:Draw \
+forge create src/CardGame.sol:Draw \
     --rpc-url $RPC_URL \
     --private-key $DEV_PRIV_KEY \
