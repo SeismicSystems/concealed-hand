5a384bf73e8f93a704c1e9c7dc21bbdb41d910cf
Alexander Lindenbaum <lindenbaum.alexander@gmail.com>
2023-12-14 12:08:09 -0500

    handle async errors

--- client/utils.ts
+++ client/utils.ts
@@ -61,16 +61,33 @@ export async function proveHonestSelect(
     playerRandomness: bigint,
     cardPlayed: number
 ): Promise<Groth16ProofCalldata> {
-    const { proof, publicSignals } = await groth16.fullProve(
-        {
-            playerCommitment: playerCommitment.toString(),
-            roundRandomness: roundRandomness.toString(),
-            playerRandomness: playerRandomness.toString(),
-            cardPlayed: cardPlayed,
-        },
-        DRAW_WASM,
-        DRAW_ZKEY
-    );
+    const groth16Inputs = {
+        playerCommitment: playerCommitment.toString(),
+        roundRandomness: roundRandomness.toString(),
+        playerRandomness: playerRandomness.toString(),
+        cardPlayed: cardPlayed,
+    };
 
-    return exportCallDataGroth16(proof, publicSignals);
+    let proverRes, proverErr;
+    [proverRes, proverErr] = await handleAsync(
+        groth16.fullProve(groth16Inputs, DRAW_WASM, DRAW_ZKEY)
+    );
+    if (!proverRes || proverErr) {
+        console.error(
+            "Error proving draw ZKP with input signals",
+            groth16Inputs
+        );
+        process.exit(1);
+    }
+
+    let exportRes, exportErr;
+    // return exportCallDataGroth16(proverRes.proof, proverRes.publicSignals);
+    [exportRes, exportErr] = await handleAsync(
+        exportCallDataGroth16(proverRes.proof, proverRes.publicSignals)
+    );
+    if (!exportRes || exportErr) {
+        console.error("Error formatting proof and public signals");
+        process.exit(1);
+    }
+    return exportRes;
 }
